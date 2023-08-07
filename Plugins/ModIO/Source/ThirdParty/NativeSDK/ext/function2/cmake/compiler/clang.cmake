77b0eb06356d062f669e971a446763792c618419
mag1182 <mag1182@rocketmail.com>
2023-08-07 09:36:09 -0500

    Packet System

--- Plugins/ModIO/Source/ThirdParty/NativeSDK/ext/function2/cmake/compiler/clang.cmake
+++
@@ -1,8 +0,0 @@
-# Enable full warnings
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wdocumentation -pedantic -Wextra")
-
-if (FU2_WITH_NO_EXCEPTIONS)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions")
-  message(STATUS "Clang: Disabled exceptions")
-endif()
-
