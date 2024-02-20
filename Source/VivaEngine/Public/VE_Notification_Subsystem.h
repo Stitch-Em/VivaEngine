89fffc012a656796c3b62ccbaebf19d8b1617af5
SolarCookies <mag1182@rocketmail.com>
2024-02-20 08:33:44 -0600

    Worked on ID Subsystem, And Saving

---
+++ Source/VivaEngine/Public/VE_Notification_Subsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "VE_Notification_Subsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIVAENGINE_API UVE_Notification_Subsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
