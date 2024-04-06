58a1762cbc324c50ed4e22fc04e858a36af8b5c2
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-04-06 11:56:21 +0200

    Added Discord Includes
    Added the binary and Cpp/H files for adding Discord SDK dependencies into Cpp code

--- Source/VivaEngine/VivaEngine.Build.cs
+++ Source/VivaEngine/VivaEngine.Build.cs
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class VivaEngine : ModuleRules
 {
@@ -12,12 +13,21 @@ public class VivaEngine : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Get the directory path where the Discord files are located
+        string DiscordFilesDirectory = Path.Combine(ModuleDirectory, "discord-files");
+        PublicIncludePaths.Add(DiscordFilesDirectory);
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Get the path to the lib file
+        string BinariesDirectory = Path.Combine(ModuleDirectory, "../../Binaries", Target.Platform.ToString());
+        string LibFilePath = Path.Combine(BinariesDirectory, "discord_game_sdk.dll.lib");
+        PublicAdditionalLibraries.Add(LibFilePath);
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
