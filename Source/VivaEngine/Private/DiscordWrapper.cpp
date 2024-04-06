d7c1dc15df9c432f52c7eca5f6284e4ab75f46d6
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-04-06 16:34:24 +0200

    DiscordWrapper Addition
    DiscordWrapper is a Wrapper for the DiscordSDK. All Discord API related functions will get added here.
    
    Only SetDiscordStatus is present as of this time, but this is subject to change

---
+++ Source/VivaEngine/Private/DiscordWrapper.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DiscordWrapper.h"
+#include "discord.h"
+
+discord::Core* core{};
+
+// Sets default values for this component's properties
+UDiscordWrapper::UDiscordWrapper()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UDiscordWrapper::SetDiscordActivity(FString State, FString Details)
+{
+	// Make sure Discord is not required
+	// We will need to hide this Token ID in something later
+	auto result = discord::Core::Create(1030046546768711720, DiscordCreateFlags_NoRequireDiscord, &core);
+	discord::Activity activity{};
+
+	// Turn FString UE5 TCHAR into a Char array
+	char* StateChar = TCHAR_TO_UTF8(*State);
+	char* DetailsChar = TCHAR_TO_UTF8(*Details);
+
+	// This will say: Playing Viva Pinata: RtP;\n In Lush Jungle
+	activity.SetType(discord::ActivityType::Playing);
+	activity.SetState(StateChar);
+	activity.SetDetails(DetailsChar);
+	
+	core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {});
+}
+
+
+// Called when the game starts
+void UDiscordWrapper::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UDiscordWrapper::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Required every tick (Per Discord SDK Docs)
+	::core->RunCallbacks();
+}
+
